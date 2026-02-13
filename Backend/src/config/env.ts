83779ceb32e9e4356c8ee94e0dc71d594cfd0d3b
Edilson Luna <edilsonluna117@gmail.com>
2026-02-12 22:05:51 -0500

    chore: Instancia de modelo de prisma. Creacion de migracion y cliente

--- Backend/src/config/env.ts
+++ Backend/src/config/env.ts
@@ -2,9 +2,5 @@ import 'dotenv/config';
 
 export const env = {
     PORT : process.env.PORT ?? 3000,
-    DB_HOST : process.env.DB_HOST ?? '',
-    DB_USER : process.env.DB_USER ?? '',
-    DB_PASS : process.env.DB_PASS ?? '',
-    DB_NAME : process.env.DB_NAME ?? '',
-    DB_PORT : process.env.DB_PORT ?? 5433,
+    DB_URL : process.env.DB_URL ?? '',
 }
